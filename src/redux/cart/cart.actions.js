2388c1e5ad2007cd98f163791a1db44beeeb8541
Chanaka Wijethilaka <chanakas.online@gmail.com>
2021-02-04 07:20:36 +0530

    add a comment to the cart.actions.js file indicating that payload is an optional property in action object

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,4 +2,5 @@ import { CartActionTypes } from './cart.types'
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
+  // payload is optional property in action object
 })
