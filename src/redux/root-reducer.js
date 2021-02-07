0952906e70dc1f4a8361a24045c6674fb2a977da
Chanaka Wijethilaka <chanakas.online@gmail.com>
2021-02-07 13:30:20 +0530

    add redux-persist library to maintain the state intact between page reloads

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // telling redux-persist to use localStorage as my default storage
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] // reducers to persist in an array
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
